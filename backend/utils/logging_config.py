1c6e3272d18b252cd346dab45188fed0f579cb92
Akhand Pratap Tiwari <akdevstudios100@gmail.com>
2024-11-26 14:45:52 +0530

    Updated plotter.py backend. Added more logging info. Configured logging_config to not use standard streams if --noconsole is passed. TODO: Manual handling of console and standard streams

--- backend/utils/logging_config.py
+++ backend/utils/logging_config.py
@@ -1,12 +1,17 @@
 import logging
+import sys
 
 def configure_logging():
     """Configure logging for the application."""
+    log_file = 'data_handler.log'
+    handlers = [logging.FileHandler(log_file)]
+
+    # Only add StreamHandler if running with console
+    # if sys.stdout is not None:
+    #     handlers.append(logging.StreamHandler())
+    
     logging.basicConfig(
-        level=logging.INFO, 
+        level=logging.INFO,
         format='%(asctime)s - %(levelname)s: %(message)s',
-        handlers=[
-            logging.FileHandler('data_handler.log'),
-            logging.StreamHandler()
-        ]
+        handlers=handlers
     )
